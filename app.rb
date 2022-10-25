69ebe54b41d3e49a87750b889c9ab943587b3497
Maria Coitinho <brasildu@gmail.com>
2022-10-25 17:36:46 -0500

    commit tree 239510330c53392faded62de873094ee9f96996d

---
+++ app.rb
@@ -0,0 +1,28 @@
+# # app.rb
+# # puts "Hello world!"
+
+# # # app.rb
+# # puts("Hello world!")
+
+# # # app.rb
+# # puts "Hello world!"
+# # puts "Hello world!"
+# # puts "Hello world!"
+
+# # # # app.rb
+# # print "Hello world!"
+# # print "Hello world!"
+# # print "Hello world!"
+
+# # # This is the equivalent of calling:
+# p [1, 2, 3]
+# puts [1, 2, 3].inspect
+
+# ## printing nested objects
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# TESTS
+
+print "Hello World!"
+# print "Pass this test, please."
+# p [1, 2, 3]
